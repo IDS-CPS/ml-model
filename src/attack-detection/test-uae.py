a5c4fcc6c938541cab8e10c18d9b9c735d2b1a95
Fabian <13519140@std.stei.itb.ac.id>
2023-04-02 23:18:29 +0700

    calculating attack detection metrics

--- src/attack-detection/test-uae.py
+++ src/attack-detection/test-uae.py
@@ -1,7 +1,13 @@
+from datetime import datetime
 import pandas as pd
 import numpy as np
 import tensorflow as tf
 import joblib
+
+def count_time(start, end):
+    start_datetime = datetime.strptime(start, "%d/%m/%Y %I:%M:%S %p")
+    end_datetime = datetime.strptime(end, "%d/%m/%Y %I:%M:%S %p")
+    return end_datetime - start_datetime
 
 model = tf.keras.models.load_model('model/v2/uae')
 
@@ -9,6 +15,8 @@ attack_df = pd.read_csv("dataset/swat-attack.csv", delimiter=";", decimal=",")
 attack_df.columns = [column.strip() for column in attack_df.columns]
 attack_df = attack_df.set_index("Timestamp")
 attack_df = attack_df[::5]
+
+attack_df = attack_df[:1000]
 
 features_dropped = ["AIT201", "AIT202", "AIT203", "P201", "AIT401",
 "AIT402", "AIT501", "AIT502", 'AIT503', "AIT504", "FIT503", "FIT504",
@@ -22,45 +30,60 @@ attack_data = scaler.transform(attack_df.drop("Normal/Attack", axis=1))
 window_size = 10
 target_size = 10
 
-threshold = 50
+threshold = 6
 anomaly_counter = 0
 time_window_threshold = 6
 
 mean = np.load("npy/uae/mean.npy")
 std = np.load("npy/uae/std.npy")
 is_attack_period = False
-attack_number = 0
 start_period = ""
 end_period = ""
-attacks = []
+
 attack_dict = dict()
+normal_dict = dict()
+attack_number = 0
+normal_number = 1
+
+normal_dict[normal_number] = {
+    "start_period": attack_df.index[window_size],
+    "false_alarms": []
+}
+
 for i in range (len(attack_data)//window_size-1):
     start_index = window_size * i
     end_index = start_index + window_size
     input_window = attack_data[start_index:start_index+window_size]
     target_window = attack_data[end_index:end_index+target_size]
 
-    prediction = model.predict(input_window.reshape(1, window_size, -1)).reshape((target_window.shape[0], target_window.shape[1]))
+    prediction = model.predict(input_window.reshape(1, window_size, -1), verbose=0).reshape((target_window.shape[0], target_window.shape[1]))
     error = np.abs(prediction - target_window)
     z_score_all = np.abs(error-mean)/std
     z_score_max = np.nanmax(z_score_all, axis=1)
 
     attack_label = attack_df.iloc[end_index:end_index+target_size]['Normal/Attack'].values
-    print(attack_label)
-    print(z_score_max)
 
     for j in range(len(z_score_max)):
-        print(attack_label[j])
         if (not is_attack_period and attack_label[j] == 'Attack'):
             is_attack_period = True
             start_period = attack_df.index[end_index+j]
             attack_number += 1
-            attack_dict[attack_number] = []
+            attack_dict[attack_number] = {
+                "start_period": start_period,
+                "attacks": []
+            }
+            normal_dict[normal_number]["end_period"] = attack_df.index[end_index+j-1]
 
         if (is_attack_period and attack_label[j] == 'Normal'):
             end_period = attack_df.index[end_index+j-1]
-            attacks.append(f"{start_period} - {end_period}")
+            attack_dict[attack_number]["end_period"] = end_period
             is_attack_period = False
+            normal_number += 1
+            normal_dict[normal_number] = {
+                "start_period": attack_df.index[end_index+j],
+                "false_alarms": []
+            }
+
 
         if z_score_max[j] > threshold:
             consecutive_counter += 1
@@ -70,24 +93,69 @@ for i in range (len(attack_data)//window_size-1):
 
         if consecutive_counter > time_window_threshold:
             consecutive_counter = 0
-            attack_dict[attack_number].append(f"{attack_df.index[end_index]} - {attack_df.index[end_index+target_size-1]}")
-            print("Attack detected in window {} - {} at time {} with anomalies z-scores {}".format(attack_df.index[end_index], attack_df.index[end_index+target_size-1], attack_df.index[end_index+j], z_score_max))
+            if 'Attack' in attack_label:
+                attack_dict[attack_number]["attacks"].append({
+                    "window_start": attack_df.index[end_index],
+                    "window_end": attack_df.index[end_index+target_size-1],
+                    "time_detected": attack_df.index[end_index+j],
+                    "z_scores": z_score_max
+                })
+            else:
+                normal_dict[normal_number]["false_alarms"].append({
+                    "window_start": attack_df.index[end_index],
+                    "window_end": attack_df.index[end_index+target_size-1],
+                    "time_detected": attack_df.index[end_index+j],
+                    "z_scores": z_score_max
+                })
 
-print("Total attacks: {}".format(attack_number))
-print("Attack Periods: {}".format(attacks))
+if is_attack_period:
+    attack_dict[attack_number]["end_period"] = attack_df.index[len(attack_data)-1]
+
+if not is_attack_period:
+    normal_dict[normal_number]["end_period"] = attack_df.index[len(attack_data)-1]
 
 attacks_detected = 0
-print(attack_dict)
-
+true_positive_count = 0
 for i in range(1, attack_number+1):
-    if len(attack_dict[i] > 0):
-        print("Attack {}: {}".format(i, attack_dict[i]))
+    print("Attack {}".format(i))
+    print("Attack Period : {}-{}".format(attack_dict[i]["start_period"], attack_dict[i]["end_period"]))
+    if len(attack_dict[i]["attacks"]) > 0:
+        attacks = attack_dict[i]["attacks"]
         attacks_detected += 1
-
-# for key, value in attack_dict:
-#     if len(value) > 0:
-#         print("Attack {}: {}".format(key, value))
-#         attacks_detected += 1
+        print("Detection speed: {}".format(count_time(attack_dict[i]["start_period"].strip(), attacks[0]["time_detected"].strip())))
+        print("Time period of attack that is detected: {} - {} ({})".format(
+            attacks[0]["window_start"].strip(), 
+            attacks[len(attacks)-1]["window_end"].strip(),
+            count_time(attacks[0]["window_start"].strip(), attacks[len(attacks)-1]["window_end"].strip())
+        ))
+        true_positive_count += len(attacks)
+        for attack in attacks:
+            print("Window: {} - {}".format(attack["window_start"].strip(), attack["window_end"].strip()))
+            print("Time Detected: {}".format(attack["time_detected"]))
+            print("Z Scores: {}".format(attack["z_scores"]))
+    print()
+    print()
 
 print("Attacks Detected: {} out of {}".format(attacks_detected, attack_number))
 print("Anomaly counter: {}".format(anomaly_counter))
+
+false_alarm_count = 0
+for i in range(1, normal_number+1):
+    print("Normal Period {}: {} - {}".format(i, normal_dict[i]["start_period"], normal_dict[i]["end_period"]))
+    if len(normal_dict[i]["false_alarms"]) > 0:
+        false_alarms = normal_dict[i]["false_alarms"]
+        false_alarm_count += len(false_alarms)
+        
+        for false_alarm in false_alarms:
+            print("Window: {} - {}".format(false_alarm["window_start"].strip(), false_alarm["window_end"].strip()))
+            print("Time Detected: {}".format(false_alarm["time_detected"]))
+            # print("Z Scores: {}".format(false_alarm["z_scores"]))
+    print()
+    print()
+
+print("True Positive Count: {}".format(true_positive_count))
+print("False Alarm Count: {}".format(false_alarm_count))
+
+# Notes for tomorrow:
+# - Hitung percentage yang kedetect: detik positive yg kedetect/ total waktu yg positive
+# - Hitung false alarm: detik false alarm/total waktu keseluruhan data
--- src/pca/pca.py
+++ src/pca/pca.py
@@ -1,40 +1,57 @@
 import pandas as pd
 import numpy as np
+import joblib
 
 from argparse import ArgumentParser
 from scipy import stats
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.decomposition import PCA
 
-train_df = pd.read_csv("dataset/swat-2015-data.csv", delimiter=";", decimal=",")
-train_df = train_df[16000:]
-train_df = train_df.drop("Normal/Attack", axis=1)
-train_df = train_df.drop("Timestamp", axis=1)
-train_df = train_df[::5]
+df = pd.read_csv("dataset/swat-2015-data.csv", delimiter=";", decimal=",")
+df = df[16000:]
+df = df.drop("Normal/Attack", axis=1)
+df = df.drop("Timestamp", axis=1)
+df = df[::5]
 
 features_dropped = ["AIT201", "AIT202", "AIT203", "P201", "AIT401",
 "AIT402", "AIT501", "AIT502", 'AIT503', "AIT504", "FIT503", "FIT504",
 "PIT501", "PIT502", "PIT503"]
 
-train_df = train_df.drop(columns=features_dropped)
+df = df.drop(columns=features_dropped)
+
+n = len(df)
+train_df = df[0:int(n*0.8)]
+test_df = df[int(n*0.8):]
+
+print("Features used: ", df.columns)
+print(len(df.columns))
 
 scaler = MinMaxScaler()
-scaler = scaler.fit(train_df)
-scaled_features = scaler.transform(train_df)
-scaled_train_df = pd.DataFrame(scaled_features, index=train_df.index, columns=train_df.columns)
+scaler = scaler.fit(df)
+joblib.dump(scaler, "scaler/pca.gz")
 
+train_data = scaler.transform(train_df)
+test_data = scaler.transform(test_df)
+
+# Fit PCA to training data
+pca = PCA(n_components=13)
+pca.fit(test_data)
+train_pca = pca.transform(test_data)
+train_recon = pca.inverse_transform(train_pca)
+error = np.abs(train_recon - test_data)
+
+e_mean = np.mean(error, axis=0)
+e_std = np.std(error, axis=0)
+
+# Setup attack data
 attack_df = pd.read_csv("dataset/swat-attack.csv", delimiter=";", decimal=",")
 attack_df.columns = [column.strip() for column in attack_df.columns]
 attack_df = attack_df.set_index("Timestamp")
 attack_df = attack_df[::5]
 
 attack_df = attack_df.drop(columns=features_dropped)
-
 attack_data = scaler.transform(attack_df.drop("Normal/Attack", axis=1))
 
-# Fit PCA to training data
-pca = PCA(n_components=13)
-pca.fit(scaled_train_df)
 
 window_size = 1
 threshold = 5
@@ -47,39 +64,16 @@ fp = 0
 consecutive_counter = 0
 for i in range(len(attack_df)//window_size):
     start_index = window_size * i
-    window = attack_data[start_index:start_index+window_size, :]
+    window = attack_data[start_index:start_index+window_size]
 
     window_pca = pca.transform(window)
     window_recon = pca.inverse_transform(window_pca)
     error = np.abs(window - window_recon)
-    error_mean = np.mean(error, axis=1)
-    error_std = np.std(error, axis=1)
-    z_score_all = np.abs(error - error_mean.reshape((error_mean.shape[0], 1)))/error_std.reshape((error_std.shape[0], 1))
+    z_score_all = np.abs(error - e_mean)/e_std
     z_score_max = np.nanmax(z_score_all, axis=1)
     
     attack_label = attack_df.iloc[start_index:start_index+window_size]['Normal/Attack'].values
     print(attack_label)
     print(z_score_max)
 
-    for j in range(len(z_score_max)):
-        if z_score_max[j] > threshold:
-            consecutive_counter += 1
-            anomaly_counter += 1
-        else:
-            consecutive_counter = 0
-
-        if consecutive_counter > time_window_threshold:
-            attack_counter += 1
-            consecutive_counter = 0
-            print("Attack detected in window {} - {} at time {} with anomalies in {}".format(attack_df.index[start_index+j], attack_df.index[start_index+window_size-1], attack_df.index[start_index+j], z_score_max))
-
-            if 'Attack' in attack_label:
-                print("Attack correctly detected")
-                tp += 1
-            else:
-                fp += 1
-            break
-
-print("Attack counter: {}".format(attack_counter))
-print("Anomaly counter: {}".format(anomaly_counter))
-print(tp, fp)
+    
