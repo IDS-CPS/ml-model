3bcaa1c7a6f64682b974a91fb3e1d6797989af12
Fabian <13519140@std.stei.itb.ac.id>
2023-04-04 23:47:05 +0700

    cnn edit

--- src/cnn/1d-cnn.py
+++ src/cnn/1d-cnn.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dropout, Dense, Reshape
 
 ## Plot the training history
-def plot_train_history(history, title):
+def plot_train_history(history, title, filename):
   loss = history.history['loss']
   val_loss = history.history['val_loss']
 
@@ -23,7 +23,7 @@ def plot_train_history(history, title):
   plt.title(title)
   plt.legend()
 
-  plt.savefig("plot/1d-cnn-history.png")
+  plt.savefig(filename)
 
 parser = ArgumentParser()
 parser.add_argument("-e", "--epoch", default=1, type=int)
@@ -57,31 +57,31 @@ joblib.dump(scaler, "scaler/1d-cnn.gz")
 train_data = scaler.transform(train_df)
 test_data = scaler.transform(test_df)
 
-# Generated training sequences for use in the model.
-def create_sequences(values, history_size, target_size, step):
+def create_sequences(values, history_size, target_size):
     data = []
     target = []
 
-    start_index = history_size
-
-    for i in range(start_index, len(values) -  target_size):
-      indices = range(i - history_size, i, step)
-      data.append(values[indices])
-      target.append(values[i:i+target_size])
+    for i in range(len(values)//history_size-1):
+        start_index = history_size * i
+        end_index = start_index + history_size
+        data.append(values[start_index:end_index])
+        target.append(values[end_index:end_index+target_size])
     
     return np.array(data), np.array(target)
 
+history_size = 40
+target_size = 40
 
-x_train, y_train = create_sequences(train_data, 40, 40, 1)
-x_test, y_test = create_sequences(test_data, 40, 40, 1)
+x_train, y_train = create_sequences(train_data, history_size, target_size)
+x_test, y_test = create_sequences(test_data, history_size, target_size)
 
-print("Training input shape: ", x_train.shape)
+print("Training input shape: ", x_train.shape, y_train.shape)
 
-train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-train_data = train_data.cache().shuffle(50000).batch(256).repeat()
+train_tensor = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+train_tensor = train_tensor.cache().shuffle(50000).batch(256).repeat()
 
-test_data = tf.data.Dataset.from_tensor_slices((x_test, y_test))
-test_data = test_data.cache().shuffle(50000).batch(256).repeat()
+val_tensor = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+val_tensor = val_tensor.cache().shuffle(50000).batch(256).repeat()
 
 
 model = tf.keras.models.Sequential()
@@ -105,10 +105,10 @@ model.compile(loss=tf.keras.losses.MeanSquaredError(),
                 metrics=[tf.keras.metrics.MeanAbsoluteError()])
 
 history = model.fit(
-  train_data, 
+  train_tensor, 
   epochs=args.epoch,
   steps_per_epoch=100,
-  validation_data=test_data,
+  validation_data=val_tensor,
   validation_steps=50,
   callbacks=[early_stopping]
 )
@@ -119,4 +119,25 @@ print(f"Loss: {loss}, Mean Absolute Error: {mean_error}")
 
 model.save('model/1d-cnn')
 
-plot_train_history(history, "Training vs Val Loss")
+error_arr = []
+for i in range (len(test_data)//history_size-1):
+    start_index = history_size * i
+    end_index = start_index + history_size
+    input_window = test_data[start_index:start_index+history_size]
+    target_window = test_data[end_index:end_index+target_size]
+
+    prediction = model.predict(input_window.reshape(1, history_size, -1)).reshape((target_window.shape[0], target_window.shape[1]))
+    error = np.abs(prediction - target_window)
+
+    error_arr.append(error)
+
+error_arr = np.asarray(error_arr)
+error_arr = error_arr.reshape((-1, error_arr.shape[-1]))
+
+error_mean = np.mean(error_arr, axis=0)
+error_std = np.std(error_arr, axis=0)
+
+np.save("npy/cnn/mean.py", error_mean)
+np.save("npy/cnn/std.py", error_mean)
+
+plot_train_history(history, "Training vs Val Loss", "plot/cnn.png")
