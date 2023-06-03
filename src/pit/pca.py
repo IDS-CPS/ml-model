80090046e9fe9380e756cc99e4fd81a1ae2a4b15
fadelananda <fadelananda123@gmail.com>
2023-06-03 23:00:02 +0700

    pca

--- src/pit/pca.py
+++ src/pit/pca.py
@@ -12,126 +12,35 @@ parser.add_argument("-d", "--dataset", default="dataset/swat-2015-data.csv", typ
 parser.add_argument("-w", "--window", default=10, type=int)
 parser.add_argument("-t", "--threshold", default=7, type=int)
 parser.add_argument("-th", "--timethreshold", default=6, type=int)
+parser.add_argument("-n", "--n_units", type=int)
 
 args = parser.parse_args()
 
 print(args)
 
 df = pd.read_csv(args.dataset)
-df = df.drop("timestamp", axis=1)
-
 n = len(df)
 
-features_considered = ['adc_level', 'adc_flow', 'adc_pressure_left', 'adc_pressure_right', 'level', 'flow', 'pressure_left', 'pressure_right']
-
-df = df[features_considered]
 train_df = df[0:int(n*0.8)]
-test_df = df[int(n*0.8):]
+val_df = df[int(n*0.8):]
 
 scaler = MinMaxScaler()
 scaler = scaler.fit(df)
 
 train_data = scaler.transform(train_df)
-test_data = scaler.transform(test_df)
+val_data = scaler.transform(val_df)
 
 # Fit PCA to training data
-pca = PCA(n_components=13)
+pca = PCA(n_components=args.n_units)
 pca.fit(train_data)
-train_pca = pca.transform(test_data)
-train_recon = pca.inverse_transform(train_pca)
-error = np.abs(train_recon - test_data)
+val_pca = pca.transform(val_data)
+val_pca = pca.inverse_transform(val_pca)
+error = np.abs(val_pca - val_data)
 
 e_mean = np.mean(error, axis=0)
 e_std = np.std(error, axis=0)
 
-# Setup attack data
-attack_df = pd.read_csv("dataset/swat-attack.csv", delimiter=";", decimal=",")
-attack_df.columns = [column.strip() for column in attack_df.columns]
-attack_df["Normal/Attack"] = attack_df["Normal/Attack"].replace(["A ttack"], "Attack")
-attack_df = attack_df.set_index("Timestamp")
-attack_df = attack_df[::5]
-# attack_df = attack_df[:1000]
-
-attack_df = attack_df.drop(columns=features_dropped)
-attack_data = scaler.transform(attack_df.drop("Normal/Attack", axis=1))
-
-window_size = args.window
-
-threshold = args.threshold
-time_threshold = args.timethreshold
-is_attack_period = False
-attack_number = 0
-attack_detected = set()
-
-consecutive_counter = 0
-attack_df["Prediction"] = "Normal"
-
-for i in range(len(attack_df)-window_size):
-    end_index = i + window_size
-    window = attack_data[i:end_index]
-
-    window_pca = pca.transform(window)
-    window_recon = pca.inverse_transform(window_pca)
-    error = np.abs(window - window_recon)
-
-    z_score_all = np.abs(error[-1] - e_mean)/e_std
-    z_score_max = np.nanmax(z_score_all)
-    
-    attack_label = attack_df.iloc[end_index-1]['Normal/Attack']
-
-    print(attack_label, z_score_max)
-
-    if (not is_attack_period and attack_label == 'Attack'):
-        is_attack_period = True
-        attack_number += 1
-
-    if (is_attack_period and attack_label == 'Normal'):
-        is_attack_period = False
-
-    if z_score_max > threshold:
-        consecutive_counter += 1
-
-    else:
-        if consecutive_counter > time_threshold:
-            start_attack = attack_df.index[end_index-consecutive_counter]
-            end_attack = attack_df.index[end_index]
-            attack_df.loc[start_attack:end_attack, "Prediction"] = "Attack"
-
-            if attack_label == 'Attack':
-                attack_detected.add(attack_number)
-
-        consecutive_counter = 0
-
-if consecutive_counter > time_threshold:
-    start_attack = attack_df.index[len(attack_data)-consecutive_counter-1]
-    end_attack = attack_df.index[len(attack_data)-1]
-    attack_df.loc[start_attack:end_attack, "Prediction"] = "Attack"
-
-
-attack_df = attack_df[window_size:]
-real_value = attack_df["Normal/Attack"].to_numpy()
-real_value[real_value == "Normal"] = 0
-real_value[(real_value == "Attack")] = 1
-
-predicted_value = attack_df["Prediction"].to_numpy()
-predicted_value[predicted_value == "Normal"] = 0
-predicted_value[predicted_value == "Attack"] = 1
-
-real_value = np.array(real_value, dtype=int)
-predicted_value = np.array(predicted_value, dtype=int)
-
-print(f"{len(attack_detected)} out of {attack_number} detected")
-print(attack_detected)
-
-print("Precision:", precision_score(real_value, predicted_value))
-print("Recall:", recall_score(real_value, predicted_value))
-print("F1 Score:", f1_score(real_value, predicted_value))
-fpr, tpr, thresholds = roc_curve(real_value, predicted_value)
-print("AUC:", auc(fpr, tpr))
-
-joblib.dump(scaler, f"scaler/pca-{window_size}.gz")
-np.save(f"npy/pca/mean-{window_size}", e_mean)
-np.save(f"npy/pca/std-{window_size}", e_std)
-joblib.dump(pca, f"model/pca-{window_size}")
-
-attack_df[["Normal/Attack", "Prediction"]].to_csv(f"dataset/result/pca-{window_size}-{threshold}-{time_threshold}.csv")
+joblib.dump(scaler, f"scaler/pca-{args.window_size}-{args.n_units}.gz")
+np.save(f"npy/pca/mean-{args.window_size}-{args.n_units}", e_mean)
+np.save(f"npy/pca/std-{args.window_size}-{args.n_units}", e_std)
+joblib.dump(pca, f"model/pca-{args.window_size}-{args.n_units}")
