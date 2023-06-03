9a6f5718d3395c62d3149f0b429d4c9d66325e1f
Fabian <13519140@std.stei.itb.ac.id>
2023-06-03 23:46:23 +0700

    pca

--- src/pit/pca.py
+++ src/pit/pca.py
@@ -40,7 +40,95 @@ error = np.abs(val_pca - val_data)
 e_mean = np.mean(error, axis=0)
 e_std = np.std(error, axis=0)
 
-joblib.dump(scaler, f"scaler/pca-{args.window_size}-{args.n_units}.gz")
-np.save(f"npy/pca/mean-{args.window_size}-{args.n_units}", e_mean)
-np.save(f"npy/pca/std-{args.window_size}-{args.n_units}", e_std)
-joblib.dump(pca, f"model/pca-{args.window_size}-{args.n_units}")
+attack_df = pd.read_csv("dataset/pompa-attack-3.csv")
+attack_df = attack_df[["adc_level","adc_temp","adc_flow","adc_pressure_left","adc_pressure_right","Class"]]
+
+attack_data = scaler.transform(attack_df.drop("Class", axis=1))
+
+window_size = args.window
+print(window_size)
+
+threshold = args.threshold
+time_threshold = args.timethreshold
+is_attack_period = False
+attack_number = 0
+attack_detected = set()
+
+consecutive_counter = 0
+attack_df["Prediction"] = "Normal"
+
+np.seterr(all="ignore")
+for i in range(len(attack_df)//window_size):
+    start_index = window_size * i
+    window = attack_data[start_index:start_index+window_size]
+
+    window_pca = pca.transform(window)
+    window_recon = pca.inverse_transform(window_pca)
+    error = np.abs(window - window_recon)
+    z_score_all = np.abs(error - e_mean)/e_std
+    z_score_max = np.nanmax(z_score_all, axis=1)
+    
+    attack_label = attack_df.iloc[start_index:start_index+window_size]['Class'].values
+
+    for j in range(len(z_score_max)):
+        if (not is_attack_period and attack_label[j] == 'Attack'):
+            is_attack_period = True
+            start_period = attack_df.index[start_index+j]
+            attack_number += 1
+
+        if (is_attack_period and attack_label[j] == 'Normal'):
+            end_period = attack_df.index[start_index+j-1]
+            is_attack_period = False
+
+        if z_score_max[j] > threshold:
+            consecutive_counter += 1
+        else:
+            consecutive_counter = 0
+
+        if consecutive_counter > time_threshold:
+            start_attack = attack_df.index[start_index+j-consecutive_counter]
+            end_attack = attack_df.index[start_index+j]
+            attack_df.loc[start_attack:end_attack, "Prediction"] = "Attack"
+
+            if 'Attack' in attack_label:
+                attack_detected.add(attack_number)
+
+            consecutive_counter = 0
+
+if consecutive_counter > time_threshold:
+    start_attack = attack_df.index[len(attack_data)-consecutive_counter-1]
+    end_attack = attack_df.index[len(attack_data)-1]
+    attack_df.loc[start_attack:end_attack, "Prediction"] = "Attack"
+
+if consecutive_counter > time_threshold:
+    start_attack = attack_df.index[len(attack_data)-consecutive_counter-1]
+    end_attack = attack_df.index[len(attack_data)-1]
+    attack_df.loc[start_attack:end_attack, "Prediction"] = "Attack"
+
+attack_df = attack_df[window_size:]
+real_value = attack_df["Class"].to_numpy()
+real_value[real_value == "Normal"] = 0
+real_value[(real_value == "Attack")] = 1
+
+predicted_value = attack_df["Prediction"].to_numpy()
+predicted_value[predicted_value == "Normal"] = 0
+predicted_value[predicted_value == "Attack"] = 1
+
+real_value = np.array(real_value, dtype=int)
+predicted_value = np.array(predicted_value, dtype=int)
+
+print(f"{len(attack_detected)} out of {attack_number} detected")
+print(attack_detected)
+
+print("Precision:", precision_score(real_value, predicted_value))
+print("Recall:", recall_score(real_value, predicted_value))
+print("F1 Score:", f1_score(real_value, predicted_value))
+fpr, tpr, thresholds = roc_curve(real_value, predicted_value)
+print("AUC:", auc(fpr, tpr))
+
+joblib.dump(scaler, f"scaler/pca-{args.window_size}.gz")
+np.save(f"npy/pca/mean-{args.window_size}", e_mean)
+np.save(f"npy/pca/std-{args.window_size}", e_std)
+joblib.dump(pca, f"model/pca-{args.window_size}.gz")
+
+attack_df[["Normal/Attack", "Prediction"]].to_csv(f"dataset/result/pca-{window_size}-{threshold}-{time_threshold}.csv", index=False)
--- train.sh
+++ train.sh
@@ -1,13 +1,25 @@
 source env/bin/activate
-#python src/pit/lstm.py -e 500 -d dataset/pompa-train-v3.csv -ht 5 -n 64 > logs/pit/lstm-5.log
-#python src/pit/lstm.py -e 500 -d dataset/pompa-train-v3.csv -ht 10 -n 100  > logs/pit/lstm-10.log
-#python src/pit/lstm.py -e 500 -d dataset/pompa-train-v3.csv -ht 20 -n 64  > logs/pit/lstm-20.log
-#python src/pit/lstm.py  -e 500 -d dataset/pompa-train-v3.csv -ht 40 -n 100  > logs/pit/lstm-40.log
-python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 5 -n 3 > logs/pit/pca-5.log
-python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 10 -n 3 > logs/pit/pca-10.log
-python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 20 -n 3 > logs/pit/pca-20.log
-python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 40 -n 3 > logs/pit/pca-40.log
-# python src/pit/lstm.py -e 500 -d dataset/pompa-train-v3-enhanced.csv -ht 5 -n 64 > logs/pit/lstm-enhance-5.log
-# python src/pit/lstm.py -e 500 -d dataset/pompa-train-v3-enhanced.csv -ht 10 -n 100 > logs/pit/lstm-enhance-10.log
-# python src/pit/lstm.py -e 500 -d dataset/pompa-train-v3-enhanced.csv -ht 20 -n 64 > logs/pit/lstm-enhance-20.log
-# python src/pit/lstm.py -e 500 -d dataset/pompa-train-v3-enhanced.csv -ht 40 -n 100 > logs/pit/lstm-enhance-40.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 5 -t 8 -th 5 -n 3 > logs/pit/pca-5-8-5.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 5 -t 8 -th 6 -n 3 > logs/pit/pca-5-8-6.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 5 -t 9 -th 5 -n 3 > logs/pit/pca-5-9-5.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 5 -t 9 -th 6 -n 3 > logs/pit/pca-5-9-6.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 5 -t 10 -th 5 -n 3 > logs/pit/pca-5-10-5.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 5 -t 10 -th 6 -n 3 > logs/pit/pca-5-10-6.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 10 -t 8 -th 5 -n 3 > logs/pit/pca-10-8-5.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 10 -t 8 -th 6 -n 3 > logs/pit/pca-10-8-6.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 10 -t 9 -th 5 -n 3 > logs/pit/pca-10-9-5.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 10 -t 9 -th 6 -n 3 > logs/pit/pca-10-9-6.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 10 -t 10 -th 5 -n 3 > logs/pit/pca-10-10-5.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 10 -t 10 -th 6 -n 3 > logs/pit/pca-10-10-6.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 20 -t 8 -th 5 -n 3 > logs/pit/pca-20-8-5.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 20 -t 8 -th 6 -n 3 > logs/pit/pca-20-8-6.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 20 -t 9 -th 5 -n 3 > logs/pit/pca-20-9-5.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 20 -t 9 -th 6 -n 3 > logs/pit/pca-20-9-6.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 20 -t 10 -th 5 -n 3 > logs/pit/pca-20-10-5.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 20 -t 10 -th 6 -n 3 > logs/pit/pca-20-10-6.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 40 -t 8 -th 5 -n 3 > logs/pit/pca-40-8-5.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 40 -t 8 -th 6 -n 3 > logs/pit/pca-40-8-6.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 40 -t 9 -th 5 -n 3 > logs/pit/pca-40-9-5.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 40 -t 9 -th 6 -n 3 > logs/pit/pca-40-9-6.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 40 -t 10 -th 5 -n 3 > logs/pit/pca-40-10-5.log
+python src/pit/pca.py -d dataset/pompa-train-v3.csv -w 40 -t 10 -th 6 -n 3 > logs/pit/pca-40-10-6.log
